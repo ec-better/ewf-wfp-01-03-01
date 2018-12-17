4b12010f7eb3c0d5fa3ee2e6db74bee85f55d2e0
Ricardo Rocha <ricardo.rocha@deimos.com.pt>
2018-12-17 16:44:08 +0100

    Initial commit

---
+++ src/main/app-resources/notebook/libexec/aux_functions.py
@@ -0,0 +1,74 @@
+#!/opt/anaconda/bin/python
+
+import sys
+import os
+import string
+import numpy as np
+from osgeo import gdal, ogr, osr
+
+def matrix_sum(mat1, mat2, no_data_value=None):
+    if no_data_value is not None:
+        if not isinstance(mat1, int):
+            mat1[(mat1 == no_data_value)] = 0
+        if not isinstance(mat2, int):
+            mat2[(mat2 == no_data_value)] = 0
+    return mat1 + mat2
+
+def crop_image(input_image, polygon_wkt, output_path, product_type=None):
+    dataset = None
+    crop_directory = os.path.dirname(output_path)
+    if crop_directory is not '' and not os.path.exists(crop_directory):
+        os.makedirs(crop_directory)
+    if input_image.startswith('ftp://') or input_image.startswith('http'):
+        try:
+            dataset = gdal.Open('/vsigzip//vsicurl/%s' % input_image)
+        except Exception as e:
+            print(e)
+    elif '.nc' in input_image:
+        dataset = gdal.Open('NETCDF:' + input_image + ':' + product_type)
+    no_data_value = dataset.GetRasterBand(1).GetNoDataValue()
+    polygon_ogr = ogr.CreateGeometryFromWkt(polygon_wkt)
+    envelope = polygon_ogr.GetEnvelope()
+    bounds = [envelope[0], envelope[2], envelope[1], envelope[3]]
+    gdal.Warp(output_path, dataset, format="GTiff", outputBoundsSRS='EPSG:4326', outputBounds=bounds, srcNodata=no_data_value, dstNodata=no_data_value)
+    
+def write_output_image(filepath, output_matrix, image_format, mask=None, output_projection=None, output_geotransform=None, no_data_value=None):
+    driver = gdal.GetDriverByName(image_format)
+    out_rows = np.size(output_matrix, 0)
+    out_columns = np.size(output_matrix, 1)
+    if mask is not None and mask is not 0:
+        output = driver.Create(filepath, out_rows, out_columns, 2, gdal.GDT_Float32)
+        mask_band = output.GetRasterBand(2)
+        mask_band.WriteArray(mask)
+        if no_data_value is not None:
+            output_matrix[mask > 0] = no_data_value
+    else:
+        output = driver.Create(filepath, out_rows, out_columns, 1, gdal.GDT_Float32)
+    
+    if output_projection is not None:
+        output.SetProjection(output_projection)
+    if output_geotransform is not None:
+        output.SetGeoTransform(output_geotransform)
+    
+    raster_band = output.GetRasterBand(1)
+    if no_data_value is not None:
+        raster_band.SetNoDataValue(no_data_value)
+    raster_band.WriteArray(output_matrix)
+    
+def calc_max_matrix(mat1, mat2, no_data_value=None):
+    if no_data_value is not None:
+        if not isinstance(mat1, int):
+            mat1[(mat1 == no_data_value)] = 0
+        if not isinstance(mat2, int):
+            mat2[(mat2 == no_data_value)] = 0
+    
+    return np.where(mat1 > mat2, mat1, mat2)
+
+def calc_average(matrix_list, n_matrix, no_data_value=None):
+    if not matrix_list:
+        return 0
+    result = matrix_list[0]
+    for i in range(1, n_matrix):
+        result = matrix_sum(result, matrix_list[i], no_data_value)
+    
+    return np.divide(result, (n_matrix*1.00))
---
+++ src/test/test_util.py
@@ -0,0 +1,89 @@
+#!/opt/anaconda/bin/python
+
+import sys
+import os
+import unittest
+import string
+import numpy as np
+from StringIO import StringIO
+import py_compile
+from osgeo import gdal, ogr
+
+sys.path.append('/workspace/wfp-01-03-01/src/main/app-resources/notebook/libexec')
+
+from aux_functions import matrix_sum, crop_image, write_output_image, calc_max_matrix, calc_average
+
+# Simulating the Runtime environment
+os.environ['TMPDIR'] = '/tmp'
+os.environ['_CIOP_APPLICATION_PATH'] = '/application'
+os.environ['ciop_job_nodeid'] = 'dummy'
+os.environ['ciop_wf_run_root'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'artifacts')
+
+#sys.path.append('../main/app-resources/util/')
+
+#from util import log_input
+
+class NodeATestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.mat1 = np.matrix('1, 1; 1, 1')
+        self.mat2 = np.matrix('2, 2; 2, 2')
+        self.mat3 = np.matrix('-9999, -9999; -9999, -9999')
+        self.mat4 = np.matrix('-9999, 2; 3, -9999')
+        self.mat5 = 0
+        self.mat6 = np.matrix('1, 2; 3, 4')
+        self.mat7 = np.matrix('2, 3; 1, 3')
+        self.test_img = "/workspace/data/test_image_chirps.tif"
+    
+    def test_matrix_sum(self):
+        sum1 = matrix_sum(self.mat1, self.mat2)
+        self.assertTrue((sum1 == np.matrix('3, 3; 3, 3')).all())
+    
+    def test_matrix_sum_with_no_data_value(self):
+        sum1 = matrix_sum(self.mat1, self.mat4, -9999)
+        self.assertTrue((sum1 == np.matrix('1, 3; 4, 1')).all())
+    
+    def test_matrix_sum_with_different_sizes(self):
+        sum1 = matrix_sum(self.mat1, self.mat5, -9999)
+        self.assertTrue((sum1 == self.mat1).all())
+    
+    def test_crop_image(self):
+        polygon = 'POLYGON((-30 -10, 20 -10, 20 40, -30 40, -30 -10))'
+        cropped_image_path = "output.tif"
+        crop_image(self.test_img, polygon, cropped_image_path)
+        self.assertGreaterEqual(os.path.getsize(cropped_image_path), 0)
+        os.remove('output.tif')
+    
+    def test_write_image(self):
+        matrix_rand = np.random.rand(30,30)
+        mask_rand = np.random.randint(2, size=(30,30))
+        filepath = "/workspace/wfp-01-03-01/src/test/output_test.tif"
+        write_output_image(filepath, matrix_rand, "GTiff", mask_rand)
+        self.assertGreaterEqual(os.path.getsize(filepath), 0)
+        os.remove('output_test.tif')
+        
+    def test_max_matrix(self):
+        max_matrix = calc_max_matrix(self.mat6, self.mat7)
+        self.assertTrue((max_matrix == np.matrix('2, 3; 3, 4')).all())
+    
+    def test_calc_average(self):
+        mat_list = [self.mat1, self.mat2, self.mat6, self.mat7]
+        average_matrix = calc_average(mat_list, 4)
+        print(average_matrix)
+        self.assertTrue((average_matrix == np.matrix('1.5, 2; 1.75, 2.5')).all())
+    
+    def test_max_matrix_with_zero(self):
+        max_matrix = calc_max_matrix(self.mat5, self.mat1) 
+        print(max_matrix)
+        self.assertTrue((max_matrix == self.mat1).all())
+
+    def test_compile(self):
+        try:
+          py_compile.compile('../main/app-resources/notebook/run', doraise=True)
+        except:
+          self.fail('failed to compile src/main/app-resources/notebook/run')
+ 
+if __name__ == '__main__':
+    unittest.main()
+
+
